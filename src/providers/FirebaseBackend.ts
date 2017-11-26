7aa45405911a8f0d02bdb65c67694f94b1d20459
Noel De Martin <noeldemartin@gmail.com>
2017-11-26 19:52:17 +0100

    Add firebase backend documentation

--- src/providers/FirebaseBackend.ts
+++ src/providers/FirebaseBackend.ts
@@ -18,6 +18,11 @@ import { Message }  from '../models/Message';
 
 import { Backend }  from './Backend';
 
+/**
+ * We are using two Firebase services within this wrapper:
+ * - FirebaseAuth to handle authentication
+ * - Firestore as a real-time database to store data
+ */
 @Injectable()
 export class FirebaseBackend extends Backend {
 
@@ -26,6 +31,12 @@ export class FirebaseBackend extends Backend {
     private roomObservableUnsubcriptions: Function[] = [];
 
     public init(): Promise<void> {
+
+        // This is the configuration to initialize FirebaseSDK with our app credentials
+        // It may seem like this is private information and shouldn't be present in source code,
+        // for example the API key, but since this will run with javascript, this information can
+        // easily be obtained on any application client.
+        // +info: https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
 
         FirebaseSDK.initializeApp({
             "apiKey": "AIzaSyBGITd8llbUX7emW2CxlGuNJtHHIrGXNQM",
@@ -36,6 +47,8 @@ export class FirebaseBackend extends Backend {
             "messagingSenderId": "893883273360"
         });
 
+        // FirebaseSDK will be loaded when listening for auth state, this can be a user already logged in or null.
+
         return new Promise<void>((resolve: any) => {
             FirebaseSDK.auth().onAuthStateChanged(resolve);
         });
@@ -43,6 +56,11 @@ export class FirebaseBackend extends Backend {
 
     public getCurrentUser(): Promise<User | null> {
         return new Promise((resolve, reject) => {
+
+            // First we retrieve the user from FirebaseAuth, but in case the user is logged in,
+            // we need to transform it to our model class User. To do that, we also need the username,
+            // which we are storing in Firestore (keep in mind that Firebase Auth and Firestore act as two separated services,
+            // the fact that we have a 'users' collection is only because we have implemented it as such).
 
             let currentUser = FirebaseSDK.auth().currentUser;
 
@@ -69,6 +87,10 @@ export class FirebaseBackend extends Backend {
     }
 
     public login(email: string, password: string): Promise<User> {
+
+        // Same as in getCurrentUser method, we need to transform Firebase Auth data in our
+        // model User class, and we need to retrieve the extra information from Firestore.
+
         return FirebaseSDK
             .auth()
             .signInWithEmailAndPassword(email, password)
@@ -100,6 +122,12 @@ export class FirebaseBackend extends Backend {
 
     public register(username: string, email: string, password: string): Promise<User> {
 
+        // Here is were a user is registered, after succesfully creating an account with Firebase Auth,
+        // we will create a user in our 'users' collection. Keep in mind that any validation such as username
+        // format or password length is ultimately validated server-side, in this case within Firebase console
+        // using Firestore rules. Any client-side validation can be useful for a better user experience but
+        // shouldn't be trusted as secure validation (all client-side code can be tampered with).
+
         let authUser: FirebaseUser;
 
         return FirebaseSDK
@@ -122,6 +150,9 @@ export class FirebaseBackend extends Backend {
                 return this.modelsFactory.makeUser(snapshot);
             })
             .catch((error) => {
+
+                // If anything went wrong, remove any data that has been created with this operation.
+
                 return (
                     authUser ? authUser.delete() : Promise.resolve()
                 ).then(() => {
@@ -135,6 +166,13 @@ export class FirebaseBackend extends Backend {
     }
 
     public createRoom(user: User, topic: string, members: string[]): Promise<Room> {
+
+        // This is a simple CRUD operation without much else to add, the only points worth mentioning
+        // is that firebase members should be user auth Ids, and none should be duplicated. This also
+        // can be validated server-side using Firestore rules. Also notice how the 'members' field is
+        // an object instead of what could be a simple array. This is because Firestore does not support
+        // querying array items, so it would make some operations difficult.
+        // source: https://stackoverflow.com/questions/46849222/firestore-query-by-item-in-array-of-document
 
         let firebaseMembers = {};
 
@@ -205,6 +243,12 @@ export class FirebaseBackend extends Backend {
 
     public observeUserRooms(user: User): Observable<Room[]> {
 
+        // In order to leverage real-time functionality in an easy way, the onSnapshot callbacks
+        // from Firestore are encapsulated inside an Observable instance. This is a design choice
+        // and could be done differently, but it is usually a good approach since working with observables
+        // is easier than working with callbacks. That is precisely the reason why observables and promises
+        // were created in the first place.
+
         let roomsSubject = new BehaviorSubject<Room[]>([]);
         let rooms: Room[] = [];
         let unsubscribe =
@@ -218,6 +262,9 @@ export class FirebaseBackend extends Backend {
 
                         let room = this.modelsFactory.makeRoom(change.doc);
                         let memberAuthIds = Object.keys(change.doc.data()['members']);
+
+                        // Members are loaded before creating messages because messages have an author field
+                        // which requires the user to be loaded as a model User class.
 
                         this.loadUsers(memberAuthIds)
                             .then(() => {
@@ -256,6 +303,10 @@ export class FirebaseBackend extends Backend {
 
     public unsubscribeRoomsObservable(roomsObservable: Observable<Room[]>): void {
 
+        // To prevent memory-leaks, it is important to always unsubscribe from observables. Although in this
+        // application we are not having into account many scenarios due to the nature of the workshop, keep
+        // in mind a real application should be more thorough about the usage of observables.
+
         let index = this.roomObservables.indexOf(roomsObservable);
 
         if (index !== -1) {
@@ -267,6 +318,9 @@ export class FirebaseBackend extends Backend {
     }
 
     private loadUsers(memberAuthIds: string[]): Promise<void> {
+
+        // This method queries users from Firestore to have them loaded in memory any time a model User class
+        // is necessary.
 
         let unloadedMemberAuthIds: string[] = memberAuthIds.filter((memberAuthId: string) => {
             return !this.modelsFactory.hasLoadedUser(memberAuthId);
@@ -281,6 +335,13 @@ export class FirebaseBackend extends Backend {
     }
 
     private updateRoomMessages(room: Room, snapshot: Firestore.QuerySnapshot): void {
+
+        // This is another design choice. Room messages could also have been implemented with an observable,
+        // but it was chosen to use an imperative approach with listeners on the Room class in order to simplify
+        // the implementation. The complexity of handling observables inside observables may not be something tribial
+        // and more memory leaks can appear. Always keep in mind the requirements of the project and reduce complexity
+        // as much as possible.
+
         snapshot.docChanges.forEach((change: Firestore.DocumentChange) => {
             switch (change.type) {
                 case 'added':
@@ -303,9 +364,13 @@ export class FirebaseBackend extends Backend {
                     break;
             }
         });
+
     }
 
     private findUsers(field: string, values: any[]): Promise<User[]> {
+
+        // Find users matching a field from an array of values.
+
         return new Promise<User[]>((resolve, reject) => {
 
             let count = values.length;
@@ -348,6 +413,11 @@ export class FirebaseBackend extends Backend {
 
 }
 
+/**
+ * This class is used to encapsulate the transformation from a Firestore document into this application's model classes. It
+ * also stores references to loaded users to be used without the need to query them multiple times. Keep into account that a
+ * real application should handle any user updates and invalidate this cache if necessary.
+ */
 class ModelsFactory {
 
     private users: {
